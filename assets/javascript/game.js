f020f266723e847c61880d7d9416c1bacfc54487
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-09-03 15:22:10 -0700

    Final version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,24 +9,35 @@ var enemyName;
 var playerHealthPoint;
 var enemyHealthPoint;
 var playerPower = 8;
-var enemyPower = 25;
+var enemyPower;
+var remainedEnemies = 3;
 var chooseYourPlayer = true;
-var imgArray = [$("#owk"), $("#ls"), $("#ds"), $("#dm")];
+var defenderInArea = false;
+var characterArray = [$("#owk"), $("#ls"), $("#ds"), $("#dm")];
+$("#owk").attr("enemyPower", 10);
+$("#ls").attr("enemyPower", 15);
+$("#ds").attr("enemyPower", 20);
+$("#dm").attr("enemyPower", 25);
+
+function test() {
+    console.log(characterArray.length);
+}
+
 
 
 $(document).ready(function () {
     $(".imgIcon").on('click', function () {
         if (chooseYourPlayer) {
             player = $(this);
-            imgArray.splice(player.index(), 1);
+            characterArray.splice(player.index(), 1);
             $(".initImg").hide();
             $(".yourCharacter").append(player);
             playerName = $('.name', player).text();
             playerHealthPoint = $("#hp", player).text();
             $(".yourCharacter").show();
 
-            for (var i = 0; i < imgArray.length; i++) {
-                $(".enemiesSection").append(imgArray[i]);
+            for (var i = 0; i < characterArray.length; i++) {
+                $(".enemiesSection").append(characterArray[i]);
             }
 
             $(".enemiesSection").show();
@@ -34,30 +45,63 @@ $(document).ready(function () {
         } else {
             enemy = $(this);
             $(".defenderSection").append(enemy);
+            $(".enemiesSection").css("pointer-events", "none");
             enemyName = $('.name', enemy).text();
+            enemyPower = enemy.attr("enemyPower");
             enemyHealthPoint = $("#hp", enemy).text();
+            defenderInArea = true;
             $(".defenderSection").show();
             $(".fightSection").show();
         }
     });
 
     $("#buttonAttack").on('click', function () {
-            $("#hp", enemy).text(enemyHealthPoint -= playerPower);
-            $("#hp", player).text(playerHealthPoint -= enemyPower);
-            $('#info1').text(`You attacked ${enemyName} for ${playerPower} damage`);
-            playerPower += 8;
-            $('#info2').text(`${enemyName} attacked you for ${enemyPower} damage`); 
-            if (playerHealthPoint < 0 || enemyHealthPoint < 0) {
-            if (playerHealthPoint >= enemyHealthPoint) {
-                $(".defenderSection").hide();
+        test();
+        if (!defenderInArea) {
+            $("#info1").text("No enemy here");
+            return;
+        }
+        $("#hp", enemy).text(enemyHealthPoint -= playerPower);
+        $("#hp", player).text(playerHealthPoint -= enemyPower);
+        $('#info1').text(`You attacked ${enemyName} for ${playerPower} damage`);
+        playerPower += 8;
+        $('#info2').text(`${enemyName} attacked you for ${enemyPower} damage`);
+        if (playerHealthPoint < 0 || enemyHealthPoint < 0) {
+            if (playerHealthPoint >= enemyHealthPoint && playerHealthPoint >= 0) {
+                $(enemy).hide();
+                defenderInArea = false;
+                remainedEnemies--;
                 $("#info1").text(`You have defeated ${enemyName}, you can choose to fight another enemy`);
                 $("#info2").text("");
+                $(".enemiesSection").css("pointer-events", "all");
+                if (remainedEnemies == 0) {
+                    $("#buttonAttack").hide();
+                    $("#info1").text("YOU WON!!!  GAME OVER!!!");
+                    $(".controlArea").append(createRestartButton());
+                }
             } else {
+                $("#buttonAttack").prop("disabled", true);
                 $("#info1").text(`You been defeated by ${enemyName}... GAME OVER!!!`);
                 $("#info2").text("");
-                $(".controlArea").append("<button class='bordered'>Restart</Button>");
+                $(".controlArea").append(createRestartButton());
             }
         }
     });
+
+    function createRestartButton() {
+        var $btn = $('<button/>', {
+            type: 'button',
+            text: 'Restart',
+            id: 'restart',
+            class: 'bordered'
+        }).click(restart);
+
+        return $btn;
+    }
+
+    function restart() {
+        location.reload();
+    }
+
 });
 
